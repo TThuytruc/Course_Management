2b67edae4c219869e03af9a1e9f716cb5ad8c531
21120537 <145359313+THAQuanttt@users.noreply.github.com>
2023-12-20 15:29:24 +0700

    commit at 20/12/2023

--- src/public/js/admin_course.js
+++ src/public/js/admin_course.js
@@ -1,3 +1,36 @@
+function DeleteAll(table,id)
+{
+  console.log("delete");
+  const delete_description= document.getElementById('delete_description');
+  delete_description.textContent=`Are you sure to delete All ${table} ?`;
+  document.getElementById('courseIdInput').value = id;
+  document.getElementById('personIdInput').value = table;
+  // Show the Bootstrap modal
+  $('#modal-course-delete').modal('show');
+
+}
+
+function DeleteAcction()
+{
+  const id= document.getElementById('courseIdInput').value;
+  const table=document.getElementById('personIdInput').value ;
+  console.log(id);
+  console.log(table);
+  if(table==='student')
+  {
+    DeleteAllStudent(id);
+  }
+  else
+  {
+    DeleteAllTeacher(id);
+  }
+}
+function CloseDialog()
+{
+  $('#modal-course-delete').modal('hide');
+}
+  // Show the Bootstrap modal
+
 async function DeleteAllTeacher(id_course)
 {
     var tbody = document.getElementById("list_teacher");
