a006cdeb1ff1752d3c8f672c5e36f7ffdb907f53
Nguyen Ngoc Thien <nthien2803@gmail.com>
2023-12-28 16:53:11 +0700

    Fix bug submission

--- src/public/js/student_submission.js
+++ src/public/js/student_submission.js
@@ -50,6 +50,7 @@ function SaveSubmission(user_id, exercise_id, exercise_name, course_name,course_
                 buttonCancel.disabled =true;
 
                 alert("submitted successfully")
+                window.location.reload();
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -125,6 +126,7 @@ async function DeleteAcction(user_id,exercise_id,exercise_name,course_name,nameF
     buttonCancel.disabled =false;
 
     $('#modal-course-delete').modal('hide');
+    window.location.reload();
     const data={user_id:user_id,exercise_id:exercise_id,exercise_name:exercise_name,course_name:course_name,nameFileSubmit:nameFileSubmit,course_id:course_id}
         
     const jsonData = JSON.stringify(data);
