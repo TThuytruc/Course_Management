074adb92265dd13882ba3844beff7768bb4b1b8d
NamLuong0920 <77575507+NamLuong0920@users.noreply.github.com>
2024-01-10 22:01:30 +0700

    Update admin_course.js

--- src/public/js/admin_course.js
+++ src/public/js/admin_course.js
@@ -1,8 +1,7 @@
-function DeleteAll(table,id)
-{
+function DeleteAll(table, id) {
   console.log("delete");
-  const delete_description= document.getElementById('delete_description');
-  delete_description.textContent=`Are you sure to delete All ${table} ?`;
+  const delete_description = document.getElementById('delete_description');
+  delete_description.textContent = `Are you sure to delete All ${table} ?`;
   document.getElementById('courseIdInput').value = id;
   document.getElementById('personIdInput').value = table;
   // Show the Bootstrap modal
@@ -10,71 +9,87 @@ function DeleteAll(table,id)
 
 }
 
-function DeleteAcction()
-{
-  const id= document.getElementById('courseIdInput').value;
-  const table=document.getElementById('personIdInput').value ;
+function DeleteAcction() {
+  const id = document.getElementById('courseIdInput').value;
+  const table = document.getElementById('personIdInput').value;
   console.log(id);
   console.log(table);
-  if(table==='student')
-  {
+  if (table === 'student') {
     DeleteAllStudent(id);
   }
-  else
-  {
+  else {
     DeleteAllTeacher(id);
   }
   CloseDialog();
 }
-function CloseDialog()
-{
+function CloseDialog() {
   $('#modal-course-delete').modal('hide');
 }
-  // Show the Bootstrap modal
+// Show the Bootstrap modal
 
-async function DeleteAllTeacher(id_course)
-{
-    var tbody = document.getElementById("list_teacher");
-    while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-    }
+async function DeleteAllTeacher(id_course) {
+  var tbody = document.getElementById("list_teacher");
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
 
-    const response = await fetch('/admin/deleteAllTeacher', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({id_course:id_course})
-      });
-  
-      if (response.ok) {
-        const result = await response.json();
-        console.log('Server response:', result);
-      } else {
-        console.error('Failed to send data to server');
-      }
-    console.log("Delete Teacher");
+  const response = await fetch('/admin/deleteAllTeacher', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ id_course: id_course })
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    console.log('Server response:', result);
+  } else {
+    console.error('Failed to send data to server');
+  }
+  console.log("Delete Teacher");
 }
-async function DeleteAllStudent(id_course)
-{
-    var tbody = document.getElementById("list_student");
-    while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-    }
+async function DeleteAllStudent(id_course) {
+  var tbody = document.getElementById("list_student");
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
 
-    const response = await fetch('/admin/deleteAllStudent', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({id_course:id_course})
-      });
-  
-      if (response.ok) {
-        const result = await response.json();
-        console.log('Server response:', result);
-      } else {
-        console.error('Failed to send data to server');
-      }
-    console.log("Delete Student");
+  const response = await fetch('/admin/deleteAllStudent', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ id_course: id_course })
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    console.log('Server response:', result);
+  } else {
+    console.error('Failed to send data to server');
+  }
+  console.log("Delete Student");
 }
+
+
+async function importExcel() {
+  const input = document.getElementById('file_submission');
+  const file = input.files[0];
+
+  const formData = new FormData();
+  formData.append('excelFile', file);
+
+  fetch('/import', {
+    method: 'POST',
+    body: formData
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Xử lý dữ liệu trả về từ server (nếu có)
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
