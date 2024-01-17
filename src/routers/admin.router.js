385be3c4726a0a12b9948ebe5b58211a88d1ab84
21120581 <thuytruc011258@gmail.com>
2024-01-18 01:56:45 +0700

    modify

--- src/routers/admin.router.js
+++ src/routers/admin.router.js
@@ -9,16 +9,13 @@ router.get('/course', adminController.course);
 router.get('/home', adminController.home);
 router.post('/deleteAllTeacher',adminController.DeleteAllTeacher);
 router.post('/deleteAllStudent',adminController.DeleteAllStudent);
-<<<<<<< HEAD
 router.post('/deleteUser',adminController.DeleteUserInCourse);
 router.post('/addTeacher',adminController.AddTeacherToCourse);
 router.post('/addStudent',adminController.AddStudentToCourse);
-=======
 router.post('/import', adminController.importExcelFile);
 router.post('/importTeacher', adminController.importExcelFileTeacher);
 
 
->>>>>>> 4cc285fc60211952c19b17b3852d96c3ec894f30
 // req.params.slug
 
 module.exports = router;
